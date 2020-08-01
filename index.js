3943e9bb40d4c558018d32fb2c0658bb926342a7
Tommy Groshong <tom@groshong.software>
2020-08-01 12:19:07 -0400

    Fix import error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/react-service-injector.production.min.js')
+  module.exports = require('./dist/react-decoupler.production.min.js');
 } else {
-  module.exports = require('./dist/react-service-injector.development.js')
+  module.exports = require('./dist/react-decoupler.development.js');
 }
