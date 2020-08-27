2ef306b04fcfb380875c5bc2234098842301f44d
Tommy Groshong <tom@groshong.software>
2020-08-27 18:41:33 -0400

    Cleanup and upgrade build dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import size from 'rollup-plugin-size';
 import nodeResolve from '@rollup/plugin-node-resolve';
